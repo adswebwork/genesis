625690b19ed8e80a8a67896bea44e37ba2df0747
adswebwork <andre@adswebwork.com>
2013-11-27 10:30:21 -0500

    project files for genesis

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html ng-app><head>
+<?php require('config.php'); ?>
+<title>Genesis V <?php print $version; ?></title>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.0/angular.min.js"></script>
+</head><body>
+
+	<?php
+		print '<h1>ADS Webwork</h1>';
+	?>
+
+
+
+  </body>
+</html>
+
+
